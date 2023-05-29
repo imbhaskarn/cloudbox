d9b9f5579a41733cfaa655db5c68917e16561410
Bhaskar Sharma <imbhaskaran@gmail.com>
2023-05-29 22:31:58 +0530

    initial commit

---
+++ web/pages/api/traefik.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json({
+    http: {
+      routers: {
+        traefik: {
+          entryPoints: ["https"],
+          rule: "Host(`portal.codewiser.online`)",
+          service: "traefik",
+        },
+      },
+      services: {
+        traefik: {
+          loadbalancer: {
+            servers: [
+              {
+                url: "http://localhost:3000",
+              },
+            ],
+          },
+        },
+      },
+    },
+  });
+}
