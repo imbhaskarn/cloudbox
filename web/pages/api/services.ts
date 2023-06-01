ac7fa58ebd3399f5d466fc80e4bf26e39d486b0d
Bhaskar Sharma <imbhaskaran@gmail.com>
2023-06-01 22:50:22 +0530

    set imports

--- web/pages/api/services.ts
+++ web/pages/api/services.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "@/db";
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    
+import prisma from "@/db";
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  
   return res.status(200).json({});
-}
+};
+export default handler;
