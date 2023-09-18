33601d0790c232762d893d19d59ecb3fd3d9d50d
bhaskar sharma <imbhaskaran@gmail.com>
2023-09-18 18:27:34 +0000

    traefik http provider with and ssl working

--- web/pages/api/traefik.ts
+++ web/pages/api/traefik.ts
@@ -4,22 +4,64 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   return res.status(200).json({
     http: {
       routers: {
+
+        cloudbox: {
+          entryPoints: ["https"],
+          rule: "Host(`cloudbox.ibhaskar.com`)",
+          service: "cloudbox",
+          tls: {
+            "certResolver": "myresolver"
+          },
+        },
         traefik: {
-          entryPoints: ["http"],
-          rule: "Host(`portal.localhost`)",
+          entryPoints: ["https"],
+          rule: "Host(`traefik.ibhaskar.com`)",
           service: "traefik",
+          tls: {
+            "certResolver": "myresolver"
+          },
         },
       },
       services: {
-        traefik: {
+        cloudbox: {
           loadbalancer: {
             servers: [
               {
-                url: "http://localhost:3000",
+                url: "http://128.199.30.61:3000",
               },
             ],
           },
         },
+        traefik: {
+          loadbalancer: {
+            servers: [
+              {
+                url: "http://128.199.30.61:8080",
+              },
+            ],
+          },
+        },
+      },
+
+    },
+
+    certificatesResolvers: {
+      myresolver: {
+        acme: {
+          email: 'imbhaskaran@gmail.com',  // Replace with your email
+          storage: 'acme.json',     // Store certificates in acme.json file
+          httpChallenge: {
+            entryPoint: 'web',
+          },
+        },
+      },
+    },
+    entryPoints: {
+      http: {
+        address: ':80',
+      },
+      https: {
+        address: ':443',
       },
     },
   });
