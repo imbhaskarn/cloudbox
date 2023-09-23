c43db7f9cc54c35a50c4bdf1d4959ef0a7190a7b
bhaskar sharma <imbhaskaran@gmail.com>
2023-09-23 15:16:51 +0000

    delete web

--- web/pages/api/services.ts
+++
@@ -1,7 +0,0 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-// import prisma from "@/db";
-// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  
-//   return res.status(200).json({});
-// };
-// export default handler;
